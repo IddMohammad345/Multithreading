99f1df1297bc05d09276ba8c0f1438c83240c4e6
Idd mohammad <mohammadidd345@gmail.com>
2026-08-05 17:02:39 +0530

    Runnable

--- src/com/idd/usingThread/MyThread.java
+++ src/com/idd/usingThread/MyThread.java
@@ -9,5 +9,7 @@ public class MyThread extends Thread{
     public static void main(String[] args) {
         MyThread t1=new MyThread();
         System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());
     }
 }
