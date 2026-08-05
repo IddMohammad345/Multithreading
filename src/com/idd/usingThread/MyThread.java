c505f9598710537cf06673a117e4b6212248fcfa
Idd mohammad <mohammadidd345@gmail.com>
2026-08-05 17:11:41 +0530

    TIME WAITING STATE

--- src/com/idd/usingThread/MyThread.java
+++ src/com/idd/usingThread/MyThread.java
@@ -3,15 +3,20 @@ package com.idd.usingThread;
 public class MyThread extends Thread{
     @Override
     public void run() {
-
+        System.out.println("RUNNING");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyThread t1=new MyThread();
         System.out.println(t1.getState());
         t1.start();
         System.out.println(t1.getState());
-
-        System.out.println(Thread.currentThread().getState());
+        Thread.sleep(100);
+        System.out.println(t1.getState());
     }
 }
