c745815ccb61b8859a1a8a4cace6e848224f2708
Idd mohammad <mohammadidd345@gmail.com>
2026-08-05 17:17:22 +0530

    TERMINATED state

--- src/com/idd/usingThread/MyThread.java
+++ src/com/idd/usingThread/MyThread.java
@@ -13,10 +13,12 @@ public class MyThread extends Thread{
 
     public static void main(String[] args) throws InterruptedException {
         MyThread t1=new MyThread();
-        System.out.println(t1.getState());
+        System.out.println(t1.getState()); //NEW state
         t1.start();
-        System.out.println(t1.getState());
+        System.out.println(t1.getState()); // RUNNABLE state
         Thread.sleep(100);
-        System.out.println(t1.getState());
+        System.out.println(t1.getState()); //TIMED_WAITING state
+        t1.join();
+        System.out.println(t1.getState()); //TERMINATED state
     }
 }
