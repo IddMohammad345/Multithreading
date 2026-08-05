e3671970cb39868f395f4608034b6adca2d98a99
Idd mohammad <mohammadidd345@gmail.com>
2026-08-05 18:28:04 +0530

    join() method

--- src/com/idd/usingThread/MyThread1.java
+++ src/com/idd/usingThread/MyThread1.java
@@ -4,19 +4,20 @@ public class MyThread1 extends Thread{
 
     @Override
     public void run() {
-        for (int i=0;i<=5;i++){
             try {
-                Thread.sleep(1000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(i);
-        }
+
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyThread1 t1=new MyThread1();
         t1.start();
+        t1.join();
+
+        System.out.println("Hello");
     }
 }
