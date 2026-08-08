cf62515ad859a4e1d7d3f2ff7ef1794bc0e78a2c
Idd mohammad <mohammadidd345@gmail.com>
2026-08-08 18:21:15 +0530

    adding priority for multiple threads

--- src/com/idd/usingThread/MyThread2.java
+++ src/com/idd/usingThread/MyThread2.java
@@ -19,8 +19,15 @@ public class MyThread2 extends Thread{
     }
 
     public static void main(String[] args) {
-        MyThread2 thread2=new MyThread2("Idd");
-        thread2.setPriority(Thread.MIN_PRIORITY);
-        thread2.start();
+        MyThread2 l=new MyThread2("Low priority thread");
+        MyThread2 m=new MyThread2("Medium priority thread");
+        MyThread2 h=new MyThread2("High priority thread");
+
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        h.setPriority(Thread.MAX_PRIORITY);
+        l.start();
+        m.start();
+        h.start();
     }
 }
