1cc8266033b29411ea9ba14b34fa3ee3e441ea22
Idd mohammad <mohammadidd345@gmail.com>
2026-08-08 17:58:55 +0530

    giving our own name to thread

---
+++ src/com/idd/usingThread/MyThread2.java
@@ -0,0 +1,25 @@
+package com.idd.usingThread;
+
+public class MyThread2 extends Thread{
+
+    public MyThread2(String name){
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i=0;i<=5;i++){
+            System.out.println(Thread.currentThread().getName() +" - Priority: "+ Thread.currentThread().getPriority()+ " -count " + i);
+            try {
+                Thread.sleep(100);
+            }catch (Exception e){
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread2 thread2=new MyThread2("Idd");
+        thread2.start();
+    }
+}
