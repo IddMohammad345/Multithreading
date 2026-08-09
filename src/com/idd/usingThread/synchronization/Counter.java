8ea11efaba91c1f4ee797fe57aeb8a63e1e81a73
Idd mohammad <mohammadidd345@gmail.com>
2026-08-09 16:25:04 +0530

    Synchronization keyword

---
+++ src/com/idd/usingThread/synchronization/Counter.java
@@ -0,0 +1,14 @@
+package com.idd.usingThread.synchronization;
+
+public class Counter {
+    private int count=0;
+
+    public synchronized void increament(){
+        count++;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+}
---
+++ src/com/idd/usingThread/synchronization/Test.java
@@ -0,0 +1,18 @@
+package com.idd.usingThread.synchronization;
+
+public class Test {
+    public static void main(String[] args) {
+        Counter counter=new Counter();
+        MyThread1 t1=new MyThread1(counter);
+        MyThread1 t2=new MyThread1(counter);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        }catch (Exception e){
+
+        }
+        System.out.println(counter.getCount());
+    }
+}
