807dc77132607151adde005285028f007c5da15a
Idd mohammad <mohammadidd345@gmail.com>
2026-08-13 22:14:24 +0530

    Lock

---
+++ src/com/idd/usingThread/lock/Main.java
@@ -0,0 +1,13 @@
+package com.idd.usingThread.lock;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount sbi=new BankAccount();
+        Runnable task= () -> sbi.withdrew(50);
+
+        Thread t1=new Thread(task,"Thread 1");
+        Thread t2=new Thread(task,"Thread 2");
+        t1.start();
+        t2.start();
+    }
+}
